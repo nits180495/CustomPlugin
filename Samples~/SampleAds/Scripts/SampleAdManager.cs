5d3a3e6839ed19effad14400cfd5be19b3da1bc2
nits180495 <119832325+nits180495@users.noreply.github.com>
2023-05-19 12:58:18 +0530

    Add files via upload

---
+++ Samples~/SampleAds/Scripts/SampleAdManager.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JPackage.AdsFramework;
+using UnityEngine.UI;
+using GoogleMobileAds.Api;
+
+public class SampleAdManager : MonoBehaviour
+{
+    private AdsConfigData adsConfigData;
+
+    #region Banner Ads Variables
+    [SerializeField] private BannerAdsManager bannerAdsManager;
+    [SerializeField] private Button WatchBannerAdsButton;
+    [SerializeField] private Button CloseBannerAdsButton;
+
+    #endregion Banner Ads Variables
+
+    #region Interstitial Ads Variables
+
+    [SerializeField] private InterstitialAdsManager interstitialAdsManager;
+    [SerializeField] private Button LoadInterstitialAdsButton;
+    [SerializeField] private Button WatchInterstitialAdsButton;
+
+    #endregion Interstitial Ads Variables
+
+    #region Reward Ads Variables
+
+    [SerializeField] private RewardAdsManager rewardAdsManager;
+    [SerializeField] private Button LoadRewardAdsButton;
+    [SerializeField] private Button WatchRewardAdsButton;
+
+    #endregion Reward Ads Variables
+
+    private void Awake()
+    {
+        adsConfigData = (AdsConfigData)AdsConfigData.GetConfig();
+    }
+
+    private void OnEnable()
+    {
+        WatchBannerAdsButton.onClick.AddListener(WatchBannerAds);
+        CloseBannerAdsButton.onClick.AddListener(CloseBannerAds);
+        LoadInterstitialAdsButton.onClick.AddListener(LoadInterstitialAds);
+        WatchInterstitialAdsButton.onClick.AddListener(WatchInterstitialAds);
+        LoadRewardAdsButton.onClick.AddListener(LoadRewardAds);
+        WatchRewardAdsButton.onClick.AddListener(WatchRewardAds);
+
+        InterstitialAdsManager.OnAdClosedEvent += () =>
+        {
+            WatchInterstitialAdsButton.interactable = true;
+            interstitialAdsManager.DestroyAd();
+        };
+        RewardAdsManager.OnAdClosedEvent += () =>
+        {
+            WatchRewardAdsButton.interactable = true;
+            rewardAdsManager.DestroyAd();
+        };
+    }
+
+    private void OnDisable()
+    {
+        WatchBannerAdsButton.onClick.RemoveAllListeners();
+        CloseBannerAdsButton.onClick.AddListener(CloseBannerAds);
+        LoadInterstitialAdsButton.onClick.RemoveAllListeners();
+        WatchInterstitialAdsButton.onClick.RemoveAllListeners();
+        LoadRewardAdsButton.onClick.RemoveAllListeners();
+        WatchRewardAdsButton.onClick.RemoveAllListeners();
+
+        BannerAdsManager.OnAdClosedEvent -= () =>
+        {
+            WatchBannerAdsButton.interactable = true;
+            bannerAdsManager.DestroyAd();
+        };
+
+        InterstitialAdsManager.OnAdClosedEvent -= () =>
+        {
+            WatchInterstitialAdsButton.interactable = true;
+            interstitialAdsManager.DestroyAd();
+        };
+        RewardAdsManager.OnAdClosedEvent -= () =>
+        {
+            WatchRewardAdsButton.interactable = true;
+            rewardAdsManager.DestroyAd();
+        };
+    }
+
+    #region Banner Ads
+
+    private void WatchBannerAds()
+    {
+        WatchBannerAdsButton.interactable = false;
+        CloseBannerAdsButton.interactable = true;
+        bannerAdsManager.CreateBannerView(adsConfigData.GetID(AdsConfigType.bannerAds), AdSize.Type.Standard,AdPosition.Top);
+        bannerAdsManager.ShowBannerAd();
+    }
+
+    private void CloseBannerAds()
+    {
+        CloseBannerAdsButton.interactable = false;
+        WatchBannerAdsButton.interactable = true;
+        bannerAdsManager.DestroyAd();
+    }
+
+    #endregion Banner Ads
+
+    #region Interstitial Ads
+
+    private void LoadInterstitialAds()
+    {
+        interstitialAdsManager.DestroyAd();
+        LoadInterstitialAdsButton.interactable = false;
+        WatchInterstitialAdsButton.interactable = true;
+        interstitialAdsManager.LoadInterstitialAd(adsConfigData.GetID(AdsConfigType.interstitialAds));
+    }
+
+    private void WatchInterstitialAds()
+    {
+        LoadInterstitialAdsButton.interactable = true;
+        WatchInterstitialAdsButton.interactable = false;
+        interstitialAdsManager.ShowInterstitialAd();
+    }
+
+    #endregion Interstitial Ads
+
+    #region Reward Ads
+
+    private void LoadRewardAds()
+    {
+        rewardAdsManager.DestroyAd();
+        LoadRewardAdsButton.interactable = false;
+        WatchRewardAdsButton.interactable = true;
+        rewardAdsManager.LoadRewardedAd(adsConfigData.GetID(AdsConfigType.rewardAds));
+    }
+
+    private void WatchRewardAds()
+    {
+        LoadRewardAdsButton.interactable = true;
+        WatchRewardAdsButton.interactable = false;
+        rewardAdsManager.ShowRewardedAd();
+    }
+
+    #endregion Reward Ads
+}
